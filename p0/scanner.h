4a90287b630d0d86f5c774f6ec0d84361cc456f9
Phoebe Gao <phgao1994@gmail.com>
2016-01-13 14:56:16 -0800

    pg: fix some token names

--- p0/scanner.h
+++ p0/scanner.h
@@ -30,10 +30,10 @@ typedef enum {
     T_Identifier, T_StringConstant, T_IntConstant, T_FloatConstant,
     T_BoolConstant, T_Print, T_ReadInteger, T_ReadLine,
     T_Const, T_Uniform, T_Layout, T_Continue, T_Do,
-    T_Switch, T_Case, T_Default, T_In, T_Out, T_InOut, 
+    T_Switch, T_Case, T_Default, T_In, T_Out, T_Inout, 
     T_Mat2, T_Mat3, T_Mat4, T_Vec2, T_Vec3, T_Vec4,
-    T_IVec2, T_IVec3, T_IVec4, T_BVec2, T_BVec3, T_BVec4, 
-    T_UVec2, T_UVec3, T_UVec4, T_UInt, T_Struct,
+    T_Ivec2, T_Ivec3, T_Ivec4, T_Bvec2, T_Bvec3, T_Bvec4, 
+    T_Uvec2, T_Uvec3, T_Uvec4, T_Uint, T_Struct,
     T_Inc, T_Dec,
     T_NumTokenTypes
 } TokenType;
@@ -52,10 +52,10 @@ static const char *gTokenNames[T_NumTokenTypes] = {
   "T_IntConstant", "T_FloatConstant", "T_BoolConstant", "T_Print",
   "T_ReadInteger", "T_ReadLine",
   "T_Const", "T_Uniform", "T_Layout", "T_Continue", "T_Do",
-  "T_Switch", "T_Case", "T_Default", "T_In", "T_Out", "T_InOut", 
+  "T_Switch", "T_Case", "T_Default", "T_In", "T_Out", "T_Inout", 
   "T_Mat2", "T_Mat3", "T_Mat4", "T_Vec2", "T_Vec3", "T_Vec4",
-  "T_IVec2", "T_IVec3", "T_IVec4", "T_BVec2", "T_BVec3", "T_BVec4", 
-  "T_UVec2", "T_UVec3", "T_UVec4", "T_UInt", "T_Struct",
+  "T_Ivec2", "T_Ivec3", "T_Ivec4", "T_Bvec2", "T_Bvec3", "T_Bvec4", 
+  "T_Uvec2", "T_Uvec3", "T_Uvec4", "T_Uint", "T_Struct",
   "T_Inc", "T_Dec"
 };
 
